8b5d6554b33d147bedb2181c0f68a4ce95ae61c1
htjb <harrytjbevins@yahoo.co.uk>
2025-08-11 18:06:30 +0100

    playing with array functions

---
+++ chapter5-HigherOrderFunctions/main.js
@@ -0,0 +1,43 @@
+const SCRIPTS = require('./scripts');
+
+require('./scripts.js');
+
+function filter(array, test) {
+  /* same as the .filter funciton for an array */
+  let passed = [];
+  for (let element of array) {
+    if (test(element)) {
+      passed.push(element);
+    }
+  }
+  return passed;
+}
+
+function map(array, transform){
+    /* same as .map */
+    let mapped = [];
+    for (let element of array){
+        mapped.push(transform(element));
+    }
+    return mapped;
+}
+
+function reduce(array, combine, start){
+    /* same as .reduce */
+    let current = start;
+    for (let element of array){
+        current = combine(current, element);
+    }
+    return current;
+}
+
+function characterCount(script){
+    return script.ranges.reduce((count, [from, to])=> count + (to - from), 0)
+}
+
+let rtlScripts = SCRIPTS.filter(s => s.direction == 'rtl')
+console.log(SCRIPTS.map(s=>s.name))
+
+//console.log([1, 2, 3, 4].reduce((a, b) => a + b))
+console.log(characterCount(SCRIPTS[0]))
+console.log(SCRIPTS.reduce((a, b) => characterCount(a) < characterCount(b) ? b : a))
