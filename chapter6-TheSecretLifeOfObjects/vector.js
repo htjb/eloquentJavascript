7acfff7cd7c2b3ec371df21c86856e04762cf83a
htjb <harrytjbevins@yahoo.co.uk>
2025-08-18 20:26:49 +0100

    vector example to learn about classes

---
+++ chapter6-TheSecretLifeOfObjects/vector.js
@@ -0,0 +1,24 @@
+
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    plus(vector) {
+        return new Vec(this.x + vector.x, this.y + vector.y)
+    }
+
+    minus(vector) {
+        return new Vec(this.x - vector.x, this.y - vector.y)
+    }
+
+    get length() {
+        return Math.sqrt(this.x**2 + this.y**2)
+    }
+}
+
+console.log(new Vec(4, 5).x)
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+console.log(new Vec(3, 4).length);
